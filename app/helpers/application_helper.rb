85c5a810abdac46b9c274158c8a564b6b1179009
Artem Shpakov <artshpakov@yandex.ru>
2014-12-09 15:32:37 +0300

    A smart partial helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def partial template, params={}, &block
+    if lookup_context.exists?(template, [], true)
+      render partial: template, locals: params[:locals]
+    else
+      block_given? && yield && ""
+    end
+  end
+
+
   def solid &block
     begin
       with_output_buffer { yield }
