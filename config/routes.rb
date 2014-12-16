b7508863d2eb2abc685fe0c7e5bc2c0a61149d1e
Artem Shpakov <artshpakov@yandex.ru>
2014-12-16 20:43:46 +0300

    Sphere index route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     get '/' => 'index#index'
     
     scope "/(:sphere)" do
-      get '/' => 'index#index'
+      get '/' => 'index#index', as: :sphere
 
       get '/objects(/criteria/:criteria)(/filters/*filters)'      => 'alternatives#index',  as: :list
       get '/objects/:id(/criteria/:criteria)(/filters/*filters)'  => 'alternatives#show',   as: :object
