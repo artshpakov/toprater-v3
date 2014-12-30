d7a30c4783e5b0e5e94678d12cfd18fff01456c2
Artem Shpakov <artshpakov@yandex.ru>
2014-12-30 15:01:48 +0300

    Searching

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -3,7 +3,16 @@ class LandingsController < ApplicationController
   layout false
   skip_before_action :set_locale, :set_sphere, :setup
 
+  respond_to :json
+
+
   def similar_movies
+  end
+
+  def search
+    I18n.locale       = params[:locale]
+    Sentimeta.sphere  = params[:sphere]
+    respond_with Sentimeta::Client.fetch :search, text: params[:term]
   end
 
 end
