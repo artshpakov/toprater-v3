30b335bfad05160ec9a1f141fcc831306e041375
soal <sorrow.about.alice@gmail.com>
2015-01-13 16:53:35 +0300

    Landings update

--- public/js/movies-typeahead.js
+++ public/js/movies-typeahead.js
@@ -1,27 +1,30 @@
 $(function() {
-
+  function updateList() {
+    $(".result").html("");
+    var url = "/en/movies/search.json?term=" + $("input#search").val();
+    $.ajax({
+      url: url,
+      method: "GET"
+    })
+    .fail(function(data) {
+      console.log(data);
+    })
+    .done(function(data) {
+      _.each(data.objects, function(object) {
+        $(".result").append($("<a href='/en/landings/similar/" + object.id + "'>" + object.label + " " + object.name + "</a>"));
+      });
+    });
+  }
   $("input#search").keypress(function(e) {
     if (e.which != 0) {
-      $(".result").html("");
-      var url = "/en/movies/search.json?term=" + $("input#search").val()
-      $.ajax({
-        url: url,
-        method: "GET"
-      })
-      .fail(function(data) {
-        console.log(data)
-      })
-      .done(function(data) {
-        _.each(data.objects, function(object) {
-          $(".result").append($("<a href='/en/landings/similar/" + object.id + "'>" + object.label + "</a>"));
-        })
-      });
+      updateList();
     }
-
+  });
+  $("input#search").keyup(function(e) {
+    if (e.keyCode == 8 || e.keyCode == 46) {
+      updateList();
+    }
   });
 
   $(".prettySocial").prettySocial();
-  
-
 });
-
