3261da1a0fc2d4688a6a430991e2e963f9073774
Artem Shpakov <artshpakov@yandex.ru>
2015-01-15 14:17:04 +0300

    New prod ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :user, "rbdev"
 set :deploy_to, "/home/#{ fetch :user }/#{ fetch :application }"
 set :branch, :development # TODO switch to master
 
-server '178.63.13.210', user: fetch(:user), roles: %w{web app}
+server '144.76.4.166', user: fetch(:user), roles: %w{web app}
