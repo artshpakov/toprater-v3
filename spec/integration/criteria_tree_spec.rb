5a60ddc8a4475db5fffbbe2be986ac1ffb515c6d
Eugene Korneev <ekorneeff@gmail.com>
2015-01-20 14:10:59 +0300

    Criteria Cart specs

--- spec/integration/criteria_tree_spec.rb
+++ spec/integration/criteria_tree_spec.rb
@@ -1,12 +1,13 @@
 require "spec_helper"
 
-RSpec.describe "Criteria Tree", type: :feature do
+RSpec.describe "Criteria Tree", type: :feature, js: true do
   before do
     allow(Sentimeta::Client).to receive(:spheres).and_return build_list(:sphere, 1)
     allow(Criterion).to receive(:all).and_return build_list(:criterion, 1).map { |crit| Criterion.new crit }
+    allow(Alternative).to receive(:rate).and_return []
   end
 
-  context 'page', js: true do
+  context 'page' do
     before do
       visit('/en/hotels')
     end
@@ -27,6 +28,19 @@ RSpec.describe "Criteria Tree", type: :feature do
       click_link 'Some Criterion'
       click_link 'Subcriteria'
       expect(current_path).to eq '/en/hotels/objects/criteria/subcriteria'
+      expect(page).to have_selector '.tiny.button'
+    end
+  end
+
+  context 'page with criteria' do
+    before do
+      visit '/en/hotels/objects/criteria/subcriteria'
+    end
+
+    it 'should remove criterion by click' do
+      find('.tiny.button').click
+      expect(current_path).to eq '/en/hotels/objects'
+      expect(page).to_not have_selector '.tiny.button'
     end
   end
 end
