07573fb6dcd4a2f70f5a0fb7703efc0b75fcd873
Artem Shpakov <artshpakov@yandex.ru>
2015-01-27 19:16:35 +0300

    New oauth keys

--- config/initializers/oauth.rb
+++ config/initializers/oauth.rb
@@ -1,9 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, "778009828890303", "f11dbbfe3821b63883ad5c844ddcc69b", provider_ignores_state: true, callback_path: '/auth/facebook/callback'
-
-  provider :twitter, "QICEypsZC5l5CqgdJbVT7eO4t", "xcsHAbpZ8KYaC9eWoTifypeemuoagAQe7WtsQGkw8aB7YJcMo2", provider_ignores_state: true
-
-  provider :google_oauth2, "428047194229-h8nkf9rvi1d0psbr07fvek939h75p9gc.apps.googleusercontent.com", "H26rvGIlVM1aFAN6avK15Vca", name: :google, provider_ignores_state: true
+  provider :facebook, "833040550095705", "10c86d2f8811fed0e2120352a8146e17", provider_ignores_state: true, callback_path: '/auth/facebook/callback'
+  provider :twitter, "kxa2M6tVJNKsP74H31dlPqUNe", "lDlvfUmnSbRJqdKO8AHv1pUbUortjcAzLgG9ZQRInHKZvuu5JJ", provider_ignores_state: true
+  provider :google_oauth2, "29511162814-k3dnk117e23qgp0giiokom4tov5ftifo.apps.googleusercontent.com", "iP9h9C79g8VlAcG3vMU1nwT4", name: :google, provider_ignores_state: true
 
 end
