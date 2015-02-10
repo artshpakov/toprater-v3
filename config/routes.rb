2eeb26ff56beffa209281de6843f637bf9adb02c
soal <sorrow.about.alice@gmail.com>
2015-02-10 13:11:18 +0300

    NYT landing – change routes

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
     namespace :landings do
       get '/similar-movies(/:id)' => :similar_movies
-      get '/employers(/:id)(/criteria/:criteria)(/filters/*filters)' => :employers
+      get '/nyt(/:id)(/criteria/:criteria)(/filters/*filters)' => :employers
     end
   end
 
