81a2b8604ec7e71625469ef1ad23af6f3fb904db
Artem Shpakov <artshpakov@yandex.ru>
2015-02-13 18:32:08 +0300

    Catalog annotations

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,6 +4,7 @@ class CatalogController < ApplicationController
     response = Sentimeta::Client.fetch :catalog, path: params[:path]
     if response.ok?
       @path     = response.body['path']
+      @content  = Sentimeta::Client.fetch :infotext, { lang: 'en', param: "catalog-annotation", data: { path: params[:path] }}
       @catalog  = response.body['catalog']
       @objects  = response.body['objects']
     end
