e678817db6f729f7ce7512a2fa572b27d6a40582
Artem Shpakov <artshpakov@yandex.ru>
2015-02-17 14:37:18 +0300

    Alternative's page maps

--- app/cells/components_cell.rb
+++ app/cells/components_cell.rb
@@ -1,13 +1,16 @@
 class ComponentsCell < TopraterCell
 
+  DEFAULT_ZOOM = 12 # 18 is Google's max
+
   def map args
-    if args[:path] && args[:location]
-      @path   = args[:path]
-      @title  = @path.last['label']
+    @title  = args[:title] || (args[:path].presence && args[:path].last['label'])
+    @zoom   = (args[:path].presence && args[:path].count*4) || DEFAULT_ZOOM
+    @geo    = args[:geo] || begin
       # TODO temporary
-      @geo    = JSON.parse(RestClient.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{ args[:location].split('/').last }"))['results'].first['geometry'] rescue nil
-      render
+      JSON.parse(RestClient.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{ args[:location].split('/').last }"))['results'].first['geometry']['location'] rescue nil
     end
+    p @title, @zoom, @geo
+    render
   end
 
 end
