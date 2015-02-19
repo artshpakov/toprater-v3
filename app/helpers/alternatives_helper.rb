60d56273da89be0d93e3e617152caf0a54122961
Artem Shpakov <artshpakov@yandex.ru>
2015-02-19 18:42:57 +0300

    Linking hotel medals with widgets

--- app/helpers/alternatives_helper.rb
+++ app/helpers/alternatives_helper.rb
@@ -15,4 +15,9 @@ module AlternativesHelper
   def rating value
     format '%.2f', (value+1)*2.5
   end
+
+  def widget_path options
+    "http://5.9.0.5/widget/#{ options.delete(:id) }?p=#{ options.merge(type: 1, scheme: :light).to_json }" #TODO temporary
+  end
+
 end
