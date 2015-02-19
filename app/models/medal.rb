cee6b5eb3b28cebd086d59d82928034952fe47ba
Artem Shpakov <artshpakov@yandex.ru>
2015-02-19 17:28:13 +0300

    Unifying medals

--- app/models/medal.rb
+++ app/models/medal.rb
@@ -1,20 +1,7 @@
-class Medal < OpenStruct#.new(:rank, :criteria, :filters)
+class Medal < OpenStruct
 
   def criterion
     Criterion.find(criteria.first)
   end
-
-  def location
-    return 'In world' unless filters.present?
-    return filters['city'].capitalize if filters['admin1']
-    return filters['country'].capitalize if filters['country']
-  end
-
-  def scope
-    return 'world' unless filters.present?
-    return 'local' if filters['admin1']
-    return 'country' if filters['country']
-  end
-
 
 end
