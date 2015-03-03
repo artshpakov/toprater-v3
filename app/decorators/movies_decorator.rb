29984cea307e360e01f3c771fedb2a11ae7d7c15
Artem Shpakov <artshpakov@yandex.ru>
2015-03-03 15:34:54 +0300

    Removing reviews from movies cards

--- app/decorators/movies_decorator.rb
+++ app/decorators/movies_decorator.rb
@@ -2,6 +2,15 @@ class MoviesDecorator < Draper::Decorator
 
   delegate_all
 
+
+  def name
+    "#{ object.name } (#{ object.extra['year'] })"
+  end
+
+  def genres
+    extra['genres'].map{ |dir| dir['label'] }
+  end
+
   def top_cast
     extra['actors'].first(20) rescue []
   end
