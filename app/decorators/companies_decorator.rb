15e5e59e2daec16c0ea44f837e6c337393ecc19d
Artem Shpakov <artshpakov@yandex.ru>
2015-03-10 16:25:16 +0300

    Company cards

--- app/decorators/companies_decorator.rb
+++ app/decorators/companies_decorator.rb
@@ -3,7 +3,11 @@ class CompaniesDecorator < Draper::Decorator
   delegate_all
 
   def careers
-    Hash[object.extra['careers']]
+    Hash[object.extra['careers']] rescue {}
+  end
+
+  def logo
+    object.extra['logo_url']
   end
 
 end
