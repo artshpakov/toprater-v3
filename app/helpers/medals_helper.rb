00f379dc206895866c7b119efe9b4098e9350a55
Artem Shpakov <artshpakov@yandex.ru>
2015-04-10 21:09:05 +0300

    Widget preview

--- app/helpers/medals_helper.rb
+++ app/helpers/medals_helper.rb
@@ -4,8 +4,8 @@ module MedalsHelper
     @hash ||= Digest::MD5.hexdigest("#{ salt }#{ Time.now }")[0..4]
   end
 
-  # def widget_path options
-  #   "http://localhost:9393/widget/#{ options.delete(:id) }?p=#{ options.to_json }"
-  # end
+  def widget_path options
+    "http://5.9.0.5/widget/#{ options.delete(:id) }?p=#{ options.to_json }"
+  end
 
 end
