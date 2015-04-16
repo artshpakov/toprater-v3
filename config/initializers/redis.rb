20530d10a296936b3f2811efbfdbfa2889acc048
Eugene Korneev <ekorneeff@gmail.com>
2015-04-16 16:08:36 +0300

    Redis from env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,7 @@
-redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env].symbolize_keys
+redis_config = if ENV['REDIS_HOST'] && ENV['REDIS_PORT']
+  { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'] }
+else
+  YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env].symbolize_keys
+end
+
 KeyValue = Redis.new redis_config
